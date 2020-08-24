9db6302e868998f69b502a7dfc8cbaa7dfdd1ca4
Matias <matias_corvalan@outlook.com>
2020-08-24 19:07:41 -0300

    .env creado

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountryRepository } from './country.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CountryRepository]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      synchronize: true,
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: Number(process.env.DATABASE_PORT) || 5432,
+      username: process.env.DATABASE_USER ||'postgres',
+      password: process.env.DATABASE_PASSSWORD ||'postgres',
+      database: process.env.DATABASE_DATABASE ||'postgres',
+      synchronize: Boolean(process.env.DATABASE_SINCRONIZE)|| true,
       entities: ["dist/**/*.entity{.ts,.js}"],
     }),
+    ConfigModule.forRoot(),
 ],
   controllers: [AppController],
   providers: [AppService],
