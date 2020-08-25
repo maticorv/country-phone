4711422fe971222c18f160f7d7e14de98e623e9b
Matias <matias_corvalan@outlook.com>
2020-08-25 09:18:41 -0300

    geoiphabilitada

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CountryRepository } from './country.repository';
-// var geoip = require('../node_modules/geoip-country');
+var geoip = require('./node_modules/geoip-country');
 
 @Injectable()
 export class AppService {
@@ -14,13 +14,13 @@ export class AppService {
   async getPhoneCode(ip): Promise<number> {
     if (ip.substr(0, 7) == "::ffff:" || ip.substr(0, 3) == "::1") {
       ip = ip.substr(7)
-      // const iso = geoip.lookup('191.82.126.180').country;
+      const iso = geoip.lookup('191.82.126.180').country;
 
-      const country = (await this.countryRepository.getPhoneCodeByIso('AR')).phonecode;
+      const country = (await this.countryRepository.getPhoneCodeByIso(iso)).phonecode;
       return country;
     }
-    // const iso = geoip.lookup(ip).country;
-    const country = (await this.countryRepository.getPhoneCodeByIso('AR')).phonecode;
+    const iso = geoip.lookup(ip).country;
+    const country = (await this.countryRepository.getPhoneCodeByIso(iso)).phonecode;
     return country;
   }
 }
