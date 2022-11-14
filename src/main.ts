ca5ce97ae4330e70f87a2802c10d3473a090d6d7
Matías Corvalán <matias.corvalan24@gmail.com>
2022-11-14 17:46:28 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   console.log('process.env.PORT :>> ', process.env.PORT);
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
